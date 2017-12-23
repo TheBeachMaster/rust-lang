5f926909e608ffa348411bfce07f4b5e1b11889c
TheBeachmaster <otienoken@outlook.com>
2017-12-23 22:26:35 +0300

    Managed to get user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
 fn main() {
     println!("Hello, world!");
+    println!("Guess The Number");
+    println!("Enter the number");
+
+    let mut guess =  String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to get input");
+    println!("You guessed: {}", guess);
 }
