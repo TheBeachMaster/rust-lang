1eff5f3f3de4ff474a059efa64042f35a7bd9f27
TheBeachmaster <otienoken@outlook.com>
2017-12-23 22:50:23 +0300

    First part of the guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,19 @@
+extern crate rand;
+
+
 use std::io;
+use rand::Rng;
+
 fn main() {
     println!("Hello, world!");
-    println!("Guess The Number");
     println!("Enter the number");
+
+    let sec_num = rand::thread_rng().gen_range(1, 101);
 
     let mut guess =  String::new();
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to get input");
     println!("You guessed: {}", guess);
+    println!("The Secret Number was : {}", sec_num);
 }
